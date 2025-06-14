4a209d2756e0bc128fe41fb95c6bd861558866d0
EzeFernandez <gonzal0@live.com.ar>
2025-06-13 21:50:54 -0300

    feat: Se crea un Form para consulta si la Nueva Consulta es por primera vez o es para añadir una a las ya existentes.

---
+++ PetShop_2025/UI_Veterinario/SelecionarMascota.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PetShop_2025.UI_Veterinario
+{
+    public partial class SelecionarMascota : Form
+    {
+        private BE.Mascota mascotaSeleccionada;
+        private BE.Cliente clienteSeleccionado;
+
+        public SelecionarMascota(BE.Mascota mascota, BE.Cliente cliente)
+        {
+            InitializeComponent();
+            rbtnNuevoIngreso.Checked = true;
+            rbtnSeleccionarMascota.Checked = false;
+
+            this.mascotaSeleccionada = mascota ?? new BE.Mascota();
+            this.clienteSeleccionado = cliente ?? new BE.Cliente();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Form formAtenderMascota = new UI_Veterinario.AtenderMascota();
+            this.Hide();
+            formAtenderMascota.ShowDialog();
+
+        }
+
+        private void btnContinuar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
