b051de679d0fcc48007110abf8078473a1784814
NicolasAbuin99 <nicolasabuin19@gmail.com>
2025-06-29 20:11:43 -0300

    Add files via upload

--- PetShop_2025/UI_Administrador/FormEditarUsuario.Designer.cs
+++ PetShop_2025/UI_Administrador/FormEditarUsuario.Designer.cs
@@ -30,6 +30,18 @@
         {
             this.btnSalir = new System.Windows.Forms.Button();
             this.lbl_Usuario = new System.Windows.Forms.Label();
+            this.GuardarCambios = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numPerfilUsuario = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numPerfilUsuario)).BeginInit();
             this.SuspendLayout();
             // 
             // btnSalir
@@ -45,21 +57,123 @@
             // lbl_Usuario
             // 
             this.lbl_Usuario.AutoSize = true;
-            this.lbl_Usuario.Location = new System.Drawing.Point(329, 143);
+            this.lbl_Usuario.Location = new System.Drawing.Point(324, 23);
             this.lbl_Usuario.Name = "lbl_Usuario";
             this.lbl_Usuario.Size = new System.Drawing.Size(99, 13);
             this.lbl_Usuario.TabIndex = 1;
             this.lbl_Usuario.Text = "EDITAR USUARIO";
+            // 
+            // GuardarCambios
+            // 
+            this.GuardarCambios.Location = new System.Drawing.Point(348, 316);
+            this.GuardarCambios.Name = "GuardarCambios";
+            this.GuardarCambios.Size = new System.Drawing.Size(75, 23);
+            this.GuardarCambios.TabIndex = 2;
+            this.GuardarCambios.Text = "Guardar Cambios";
+            this.GuardarCambios.UseVisualStyleBackColor = true;
+            this.GuardarCambios.Click += new System.EventHandler(this.GuardarCambios_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(240, 88);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(240, 131);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 4;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(240, 184);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(240, 233);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(100, 20);
+            this.txtContraseña.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(138, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(138, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Apellido";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(138, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(135, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Contraseña";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(138, 292);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Perfil de Usuario";
+            // 
+            // numPerfilUsuario
+            // 
+            this.numPerfilUsuario.Location = new System.Drawing.Point(240, 290);
+            this.numPerfilUsuario.Name = "numPerfilUsuario";
+            this.numPerfilUsuario.Size = new System.Drawing.Size(26, 20);
+            this.numPerfilUsuario.TabIndex = 12;
             // 
             // FormEditarUsuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numPerfilUsuario);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.GuardarCambios);
             this.Controls.Add(this.lbl_Usuario);
             this.Controls.Add(this.btnSalir);
             this.Name = "FormEditarUsuario";
             this.Text = "FormEditarUsuario";
+            ((System.ComponentModel.ISupportInitialize)(this.numPerfilUsuario)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,5 +183,16 @@
 
         private System.Windows.Forms.Button btnSalir;
         private System.Windows.Forms.Label lbl_Usuario;
+        private System.Windows.Forms.Button GuardarCambios;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numPerfilUsuario;
     }
 }
--- PetShop_2025/UI_Administrador/FormEditarUsuario.cs
+++ PetShop_2025/UI_Administrador/FormEditarUsuario.cs
@@ -7,19 +7,84 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BE;
 
 namespace PetShop_2025.UI_Administrador
 {
     public partial class FormEditarUsuario : Form
     {
-        public FormEditarUsuario()
+        private BE.Usuario usuarioEditar;
+        private Form _formularioAnterior;
+        public FormEditarUsuario(Form formularioAnterior, BE.Usuario usuario)
         {
             InitializeComponent();
+            usuarioEditar = usuario;
+            CargarDatosUsuario();
         }
 
+        private void CargarDatosUsuario()
+        {
+            if (usuarioEditar != null)
+            {
+                txtNombre.Text = usuarioEditar.Nombre;
+                txtApellido.Text = usuarioEditar.Apellido;
+                txtEmail.Text = usuarioEditar.Email; 
+
+                // Por seguridad, la contraseña no se suele mostrar.
+                // Si la quieres mostrar para que el usuario pueda verla y cambiarla:
+                txtContraseña.Text = usuarioEditar.Password;
+
+                // Asegúrate de que nudPerfilUsuario.Value acepte el ID del perfil.
+                // Si Perfil.ID es 0 y nudPerfilUsuario tiene un Minimum mayor, esto podría fallar.
+                // Considera un ComboBox para perfiles si los IDs no son secuenciales o quieres mostrar nombres.
+                numPerfilUsuario.Value = usuarioEditar.Perfil.ID;
+            }
+        }
+
+        private void GuardarCambios_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ValidarCamposUsuario(); // Primero, valida los campos del formulario
+
+                // Actualiza el objeto usuario con los nuevos datos
+                usuarioEditar.Nombre = txtNombre.Text.Trim();
+                usuarioEditar.Apellido = txtApellido.Text.Trim();
+                usuarioEditar.Email = txtEmail.Text.Trim();
+                usuarioEditar.Password = txtContraseña.Text; // Actualiza la contraseña si se cambió
+
+                // Actualiza el ID del perfil. Si es un ComboBox, sería diferente.
+                usuarioEditar.Perfil.ID = (int)numPerfilUsuario.Value;
+
+                BLL.Usuario bllUsuario = new BLL.Usuario();
+                
+                bool actualizado = bllUsuario.ModificarUsuario(usuarioEditar);
+
+                if (actualizado)
+                {
+                    MessageBox.Show("Usuario actualizado correctamente.");
+                    this.DialogResult = DialogResult.OK; // Indica que la operación fue exitosa
+                    this.Close(); // Cierra el formulario actual
+                    _formularioAnterior.Show(); // Muestra el formulario anterior
+                }
+                else
+                {
+                    MessageBox.Show("No se pudo actualizar el usuario.");
+                }
+            }
+            catch (BE.ExcepcionDeNegocio exNegocio)
+            {
+                MessageBox.Show(exNegocio.Mensaje);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error inesperado: " + ex.Message);
+            }
+        }
         private void btnSalir_Click(object sender, EventArgs e)
         {
             this.Close();
+            _formularioAnterior.Show(); // Muestra el formulario anterior
         }
 
         private void ValidarCamposUsuario()
@@ -41,5 +106,7 @@ namespace PetShop_2025.UI_Administrador
                 throw new BE.ExcepcionDeNegocio("El campo 'Contraseña' del usuario es obligatorio.");
             }
         }
+
+        
     }
 }
