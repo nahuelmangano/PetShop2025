46c6e4d7f962f9f51aa2b2fb3f5276d8fcd19b58
EzeFernandez <gonzal0@live.com.ar>
2025-07-01 20:53:38 -0300

    feat: Se solicita indicar la opción correspondiente y se redirige al Form correspondiente

--- PetShop_2025/UI_Veterinario/SelecionarMascota.Designer.cs
+++ PetShop_2025/UI_Veterinario/SelecionarMascota.Designer.cs
@@ -33,6 +33,10 @@
             this.rbtnSeleccionarMascota = new System.Windows.Forms.RadioButton();
             this.btnContinuar = new System.Windows.Forms.Button();
             this.btnCancelar = new System.Windows.Forms.Button();
+            this.gBoxSeleccionarMascota = new System.Windows.Forms.GroupBox();
+            this.dgvMascotas = new System.Windows.Forms.DataGridView();
+            this.gBoxSeleccionarMascota.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMascotas)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -65,10 +69,12 @@
             this.rbtnSeleccionarMascota.TabStop = true;
             this.rbtnSeleccionarMascota.Text = "Seleccionar Mascota";
             this.rbtnSeleccionarMascota.UseVisualStyleBackColor = true;
+            this.rbtnSeleccionarMascota.CheckedChanged += new System.EventHandler(this.rbtnSeleccionarMascota_CheckedChanged);
             // 
             // btnContinuar
             // 
-            this.btnContinuar.Location = new System.Drawing.Point(322, 465);
+            this.btnContinuar.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnContinuar.Location = new System.Drawing.Point(444, 581);
             this.btnContinuar.Name = "btnContinuar";
             this.btnContinuar.Size = new System.Drawing.Size(75, 23);
             this.btnContinuar.TabIndex = 3;
@@ -78,7 +84,9 @@
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(421, 465);
+            this.btnCancelar.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancelar.Location = new System.Drawing.Point(545, 581);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(75, 23);
             this.btnCancelar.TabIndex = 4;
@@ -86,11 +94,31 @@
             this.btnCancelar.UseVisualStyleBackColor = true;
             this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
+            // gBoxSeleccionarMascota
+            // 
+            this.gBoxSeleccionarMascota.Controls.Add(this.dgvMascotas);
+            this.gBoxSeleccionarMascota.Location = new System.Drawing.Point(24, 82);
+            this.gBoxSeleccionarMascota.Name = "gBoxSeleccionarMascota";
+            this.gBoxSeleccionarMascota.Size = new System.Drawing.Size(604, 460);
+            this.gBoxSeleccionarMascota.TabIndex = 5;
+            this.gBoxSeleccionarMascota.TabStop = false;
+            this.gBoxSeleccionarMascota.Text = "Seleccionar Mascota";
+            this.gBoxSeleccionarMascota.Visible = false;
+            // 
+            // dgvMascotas
+            // 
+            this.dgvMascotas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMascotas.Location = new System.Drawing.Point(6, 19);
+            this.dgvMascotas.Name = "dgvMascotas";
+            this.dgvMascotas.Size = new System.Drawing.Size(590, 435);
+            this.dgvMascotas.TabIndex = 0;
+            // 
             // SelecionarMascota
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(508, 500);
+            this.ClientSize = new System.Drawing.Size(645, 616);
+            this.Controls.Add(this.gBoxSeleccionarMascota);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnContinuar);
             this.Controls.Add(this.rbtnSeleccionarMascota);
@@ -98,6 +126,9 @@
             this.Controls.Add(this.label1);
             this.Name = "SelecionarMascota";
             this.Text = "Nueva Consulta";
+            this.Load += new System.EventHandler(this.SelecionarMascota_Load);
+            this.gBoxSeleccionarMascota.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMascotas)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,5 +141,7 @@
         private System.Windows.Forms.RadioButton rbtnSeleccionarMascota;
         private System.Windows.Forms.Button btnContinuar;
         private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.GroupBox gBoxSeleccionarMascota;
+        private System.Windows.Forms.DataGridView dgvMascotas;
     }
 }
--- PetShop_2025/UI_Veterinario/SelecionarMascota.cs
+++ PetShop_2025/UI_Veterinario/SelecionarMascota.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BE;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,28 +14,79 @@ namespace PetShop_2025.UI_Veterinario
     public partial class SelecionarMascota : Form
     {
         private BE.Mascota mascotaSeleccionada;
-        private BE.Cliente clienteSeleccionado;
+        private int alturaGBox, alturaFormularioSinGroupBox;
 
-        public SelecionarMascota(BE.Mascota mascota, BE.Cliente cliente)
+        public SelecionarMascota()
         {
             InitializeComponent();
-            rbtnNuevoIngreso.Checked = true;
-            rbtnSeleccionarMascota.Checked = false;
-
-            this.mascotaSeleccionada = mascota ?? new BE.Mascota();
-            this.clienteSeleccionado = cliente ?? new BE.Cliente();
+            this.mascotaSeleccionada = new BE.Mascota();
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
         {
-            Form formAtenderMascota = new UI_Veterinario.AtenderMascota();
-            this.Hide();
-            formAtenderMascota.ShowDialog();
-
+            this.Close();
         }
 
         private void btnContinuar_Click(object sender, EventArgs e)
         {
+            if (rbtnNuevoIngreso.Checked)
+            {
+                var nuevaConsulta = new UI_Veterinario.NuevoIngreso(); // Nuevo ingreso 
+                this.Hide();
+                nuevaConsulta.ShowDialog();
+            }
+            else if (rbtnSeleccionarMascota.Checked)
+            {
+                if (dgvMascotas.CurrentRow == null)
+                {
+                    MessageBox.Show("Debe seleccionar una mascota.", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                var mascota = dgvMascotas.CurrentRow.DataBoundItem as BE.Mascota;
+                if (mascota != null)
+                {
+                    var nuevaConsulta = new UI_Veterinario.NuevaConsulta(mascota);
+                    this.Hide();
+                    nuevaConsulta.ShowDialog();
+                }
+            }
+
+        }
+
+        private void rbtnSeleccionarMascota_CheckedChanged(object sender, EventArgs e)
+        {
+            bool mostrar = rbtnSeleccionarMascota.Checked;
+            gBoxSeleccionarMascota.Visible = mostrar;
+
+            this.Height = mostrar
+                ? alturaFormularioSinGroupBox
+                : alturaFormularioSinGroupBox - alturaGBox;
+
+        }
+
+        private void SelecionarMascota_Load(object sender, EventArgs e)
+        {
+            this.CenterToScreen();
+
+            // Cargar mascotas al DGV
+            List<BE.Mascota> mascotas = new BLL.Mascota().ListarMascotas();
+            dgvMascotas.AutoGenerateColumns = true;
+            dgvMascotas.DataSource = mascotas;
+
+            // Configurar el DataGridView
+            dgvMascotas.Columns["ID"].Visible = false;
+            dgvMascotas.Columns["FechaNacimiento"].Visible = false;
+            dgvMascotas.Columns["Esterilizada"].Visible = false;
+
+            // Guardar altura original del formulario y del GroupBox
+            alturaFormularioSinGroupBox = this.Height;
+            alturaGBox = gBoxSeleccionarMascota.Height;
+
+            // Ocultar el GroupBox y ajustar el tamaño del formulario
+            gBoxSeleccionarMascota.Visible = false;
+            this.Height = alturaFormularioSinGroupBox - alturaGBox;
+
 
         }
     }
