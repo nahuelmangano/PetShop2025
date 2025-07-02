1eba466e4707d0ccb0504065ba876de222a3ce2d
EzeFernandez <gonzal0@live.com.ar>
2025-07-02 00:22:29 -0300

    fix: Se cambio el nombre del SP de 'ValidarUsuario' según convención

--- DAL/Usuario.cs
+++ DAL/Usuario.cs
@@ -119,7 +119,7 @@ namespace DAL
 
         public BE.Usuario ValidarUsuario(string email, string password)
         {
-            string nombreStoreProcedure = "sp_validar_usuario";
+            string nombreStoreProcedure = "usp_validar_usuario";
             SqlParameter[] parametros = new SqlParameter[2];
             Conexion objConexion = new Conexion();
             parametros[0] = objConexion.crearParametro("@Email", email);
